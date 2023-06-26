682e17f30ff2dd12b078b259298885e93a2f03c4
AdrianaRoza <adriana.roza.aka@gmail.com>
2023-06-26 10:03:34 -0400

    feat: dynamically loading the board

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TicTacToe.css';
 
 function TicTacToe() {
+  const emptyBoard = Array(9).fill("")
+  const [board, setBoard] = useState(emptyBoard)
   return (
-  <span>Jogo Da Velha</span>
+    <main>
+      <h1 className='title'>Jogo Da Velha</h1>
+
+      <div className='board'>
+        {board.map((item, index) => (
+          <div 
+            key={index}
+            className={`cell ${item}` }
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </main>
+  
   );
 }
 
